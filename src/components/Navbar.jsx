2fec1ae022ce9f6aaf9c225bd6622de00ae5ab42
Sudharsan <sudharsandharma007@gmail.com>
2025-12-29 14:34:09 +0530

    restore required navbar icon imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes, FaSearch } from "react-icons/fa";
 import "./Navbar.css";
 
 const Navbar = () => {
