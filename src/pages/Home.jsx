f452e86fe1011eb6281b80f6b6467dbc569579b3
pravalika23-b <pravali2312@gmail.com>
2026-01-03 20:07:47 +0530

    Fix import path for BestFoods component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import BestFoods from "./components/BestFoods";
+import BestFoods from "../components/BestFoods";
 
 const featuredRestaurants = [
   {
