71608a1715d463ad88887500676c0654d34ddd7e
Anirudhram2006 <anirudhram2k6@gmail.com>
2026-01-05 20:27:39 +0530

    Add shopping cart icon and count to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaSearch, FaBars, FaTimes } from "react-icons/fa";
+import { FaSearch, FaBars, FaTimes, FaShoppingCart } from "react-icons/fa";
 import { SearchContext } from "../context/SearchContext";
+import { CartContext } from "../context/CartContext";
 import "./Navbar.css";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const { setSearchTerm } = useContext(SearchContext);
+  const { cartItems } = useContext(CartContext);
   const navigate = useNavigate();
 
   const handleSearch = (e) => {
@@ -33,8 +35,20 @@ const Navbar = () => {
 
       {/* Links */}
       <div className={`z-links ${open ? "open" : ""}`}>
+        <Link to="/cart" className="cart-link" onClick={() => setOpen(false)}>
+          <FaShoppingCart />
+          {cartItems.length > 0 && (
+            <span className="cart-count">{cartItems.length}</span>
+          )}
+        </Link>
+
         <Link to="/login" onClick={() => setOpen(false)}>Log in</Link>
-        <Link to="/signup" className="signup" onClick={() => setOpen(false)}>
+
+        <Link
+          to="/signup"
+          className="signup"
+          onClick={() => setOpen(false)}
+        >
           Sign up
         </Link>
       </div>
